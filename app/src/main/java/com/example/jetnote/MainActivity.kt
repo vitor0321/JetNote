17219e9fa7fdf15118ba936e4031012e5b84a1d9
Vitor Walcker <walcker@MacBook-Pro-de-Vitor.local>
2023-02-10 09:13:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jetnote/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.jetnote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnote.ui.theme.JetNoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { MyApp {  } }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetNoteTheme {
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {  }
+}
